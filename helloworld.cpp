fc670bb6e21ae8b6472160ae7136a8d38cab75ab
Shaleen Bansal <shaleen@MacBook-Pro.local>
2020-08-14 15:01:38 +0530

    intern

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <bitset/stdc++.h>
+using namespace std;
+int main(){
+    #ifndef ONLINE_JUDGE
+    freeopen("input.txt", "r", stdin);
+    freeopen("output.txt", "r", stdin);
+    #endif
+    cout<<"hello"<<endl;
+    return 0;
+}
---
+++ random/Contest/Contest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Contest
+//
+//  Created by Shaleen Bansal on 22/06/20.
+//  Copyright © 2020 Shaleen Bansal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
